26cb01661931e80c087877e0e8669d743bedaf0d
Hungnee <hungthanh1412hz@gmail.com>
2023-09-27 22:59:07 +0700

    full fixed function

--- main.js
+++ main.js
@@ -22,17 +22,56 @@ var valueCal = document.querySelector("#value");
 var plus = document.querySelector("#plus");
 var equal = document.getElementById("equal");
 var clearVal = document.getElementById("clear");
+var except = document.getElementById("except");
+var multiply = document.getElementById("multiply");
+var divide = document.getElementById("divide");
 
 // đặt biến đệm để chứa chuỗi mà không cần dùng mảng
 var currentValue = "_";
-
+// Khởi tạo biến
 function startValue() {
   valueCal.innerHTML = currentValue;
 }
 startValue();
 
+//SIGN
 clearVal.addEventListener("click", startValue);
 
+divide.addEventListener("click", () => {
+  if (valueCal.textContent === "_") {
+    var valueCalReplaced = valueCal.textContent.replace("_", "/");
+    valueCal.innerHTML = valueCalReplaced;
+  }
+  valueCal.innerHTML += `/`;
+});
+multiply.addEventListener("click", () => {
+  if (valueCal.textContent === "_") {
+    var valueCalReplaced = valueCal.textContent.replace("_", "*");
+    valueCal.innerHTML = valueCalReplaced;
+  }
+  valueCal.innerHTML += `*`;
+});
+except.addEventListener("click", () => {
+  if (valueCal.textContent === "_") {
+    var valueCalReplaced = valueCal.textContent.replace("_", "-");
+    valueCal.innerHTML = valueCalReplaced;
+  }
+  valueCal.innerHTML += `-`;
+});
+
+plus.addEventListener("click", () => {
+  if (valueCal.textContent === "_") {
+    var valueCalReplaced = valueCal.textContent.replace("_", "1");
+    // lưu ý khi biến đã xử lý backend thì phải đưa ra giao diện
+    valueCal.innerHTML = valueCalReplaced;
+  } else valueCal.innerText += `+`;
+});
+equal.addEventListener("click", () => {
+  if (valueCal.textContent !== "_") {
+    valueCal.innerHTML = eval(valueCal.textContent);
+  }
+});
+//NUMBER
 num1.addEventListener("click", () => {
   if (valueCal.textContent === "_") {
     console.log(typeof valueCal.textContent);
@@ -41,20 +80,69 @@ num1.addEventListener("click", () => {
     valueCal.innerHTML = valueCalReplaced;
   } else valueCal.innerText += `1`;
 });
-plus.addEventListener("click", () => {
+num2.addEventListener("click", () => {
   if (valueCal.textContent === "_") {
-    var valueCalReplaced = valueCal.textContent.replace("_", "1");
+    console.log(typeof valueCal.textContent);
+    var valueCalReplaced = valueCal.textContent.replace("_", "2");
     // lưu ý khi biến đã xử lý backend thì phải đưa ra giao diện
     valueCal.innerHTML = valueCalReplaced;
-  } else valueCal.innerText += `+`;
+  } else valueCal.innerText += `2`;
 });
-num2.addEventListener("click", () => {
-  valueCal.innerHTML += `2`;
+num3.addEventListener("click", () => {
+  if (valueCal.textContent === "_") {
+    console.log(typeof valueCal.textContent);
+    var valueCalReplaced = valueCal.textContent.replace("_", "3");
+    // lưu ý khi biến đã xử lý backend thì phải đưa ra giao diện
+    valueCal.innerHTML = valueCalReplaced;
+  } else valueCal.innerText += `3`;
 });
-equal.addEventListener("click", () => {
-  if (valueCal.textContent !== "_") {
-    valueCal.innerHTML = eval(valueCal.textContent);
-  }
+num4.addEventListener("click", () => {
+  if (valueCal.textContent === "_") {
+    console.log(typeof valueCal.textContent);
+    var valueCalReplaced = valueCal.textContent.replace("_", "4");
+    // lưu ý khi biến đã xử lý backend thì phải đưa ra giao diện
+    valueCal.innerHTML = valueCalReplaced;
+  } else valueCal.innerText += `4`;
+});
+num5.addEventListener("click", () => {
+  if (valueCal.textContent === "_") {
+    console.log(typeof valueCal.textContent);
+    var valueCalReplaced = valueCal.textContent.replace("_", "5");
+    // lưu ý khi biến đã xử lý backend thì phải đưa ra giao diện
+    valueCal.innerHTML = valueCalReplaced;
+  } else valueCal.innerText += `5`;
+});
+num6.addEventListener("click", () => {
+  if (valueCal.textContent === "_") {
+    console.log(typeof valueCal.textContent);
+    var valueCalReplaced = valueCal.textContent.replace("_", "6");
+    // lưu ý khi biến đã xử lý backend thì phải đưa ra giao diện
+    valueCal.innerHTML = valueCalReplaced;
+  } else valueCal.innerText += `6`;
+});
+num7.addEventListener("click", () => {
+  if (valueCal.textContent === "_") {
+    console.log(typeof valueCal.textContent);
+    var valueCalReplaced = valueCal.textContent.replace("_", "7");
+    // lưu ý khi biến đã xử lý backend thì phải đưa ra giao diện
+    valueCal.innerHTML = valueCalReplaced;
+  } else valueCal.innerText += `7`;
+});
+num8.addEventListener("click", () => {
+  if (valueCal.textContent === "_") {
+    console.log(typeof valueCal.textContent);
+    var valueCalReplaced = valueCal.textContent.replace("_", "8");
+    // lưu ý khi biến đã xử lý backend thì phải đưa ra giao diện
+    valueCal.innerHTML = valueCalReplaced;
+  } else valueCal.innerText += `8`;
+});
+num9.addEventListener("click", () => {
+  if (valueCal.textContent === "_") {
+    console.log(typeof valueCal.textContent);
+    var valueCalReplaced = valueCal.textContent.replace("_", "9");
+    // lưu ý khi biến đã xử lý backend thì phải đưa ra giao diện
+    valueCal.innerHTML = valueCalReplaced;
+  } else valueCal.innerText += `9`;
 });
 
 // console.log(valueCal.textContent.replace("_","1"))
